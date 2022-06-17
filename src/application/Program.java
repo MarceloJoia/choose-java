d9c5ff0cf3eb8df57e9083f886d57337b6b6958d
MarceloJoia <sitejoia@hotmail.com>
2022-06-17 16:23:44 -0300

    First class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,17 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello world!");
+		Position pos = new Position(1, 5);
+		
+		System.out.println(pos);
 		
 		sc.close();
 	}
